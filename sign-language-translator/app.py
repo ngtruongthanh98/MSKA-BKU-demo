9656e29a99b7472c34160a16d4e625b21996d7f1
Luca <ngtruongthanh98@gmail.com>
2024-11-06 14:06:39 +0700

    Refactor app.py to update ngrok auth token

--- sign-language-translator/app.py
+++ sign-language-translator/app.py
@@ -12,7 +12,7 @@ def translate_route():
 
 if __name__ == '__main__':
     # auth_token = getpass.getpass("Enter your ngrok authtoken, which can be copied from https://dashboard.ngrok.com/get-started/your-authtoken: ")
-    auth_token = '2PtzIXwPLLVl1tSapKtcCyEiOGI_35HYt93wuTTwUbyiTEA3m'
+    auth_token = '2oSxR6sYqEk9YGsvagepltCTLQx_2igWYPxQQyBsDPbE8hskv'
     conf.get_default().auth_token = auth_token
 
     # Open a ngrok tunnel to the HTTP server
